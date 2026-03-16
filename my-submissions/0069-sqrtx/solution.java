b2f0a239c6bc04e8d25231e085f7466c6d84ead8
MOHIT KUMAR <111651529+mahit8171@users.noreply.github.com>
2026-03-16 18:04:06 +0000

    Sync LeetCode submission Runtime - 1 ms (97.49%), Memory - 42.5 MB (86.54%)

--- my-submissions/0069-sqrtx/solution.java
+++ my-submissions/0069-sqrtx/solution.java
@@ -1,19 +1,25 @@
 class Solution {
     public int mySqrt(int x) {
-        int left = 1, right = x, ans = 0;
+        int left = 0;
+        int right = x;
+        int ans = 0;
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
 
-            if (mid <= x / mid) {
-                ans = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+            // avoid overflow
+            if ((long) mid * mid == x) {
+                return mid;
+            }
+            else if ((long) mid * mid < x) {
+                ans = mid;        // possible answer
+                left = mid + 1;   // search right half
+            }
+            else {
+                right = mid - 1;  // search left half
             }
         }
 
         return ans;
-        
     }
 }
