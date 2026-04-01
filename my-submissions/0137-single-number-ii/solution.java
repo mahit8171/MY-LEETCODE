d81319d834c7baf8bf91ac4636df896d0dade997
MOHIT KUMAR <111651529+mahit8171@users.noreply.github.com>
2026-04-01 22:15:46 +0000

    Sync LeetCode submission Runtime - 2 ms (76.11%), Memory - 45.2 MB (81.24%)

---
+++ my-submissions/0137-single-number-ii/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            int sum = 0;
+
+            for (int num : nums) {
+                sum += (num >> i) & 1;
+            }
+
+            if (sum % 3 != 0) {
+                result |= (1 << i); //  LEFT SHIFT HERE
+            }
+        }
+
+        return result;
+    }
+}
